cf7e1deb67ce7c46216a77c8a3d203afc460b6e2
antonymburia <antonymburia10@gmail.com>
2022-03-08 01:16:23 +0300

    modify files

--- config.py
+++ config.py
@@ -31,8 +31,7 @@ class ProdConfig(Config):
         
     '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    
 
     
    
