2904da3fb076ed10491108d19a71d27a42cb046d
antonymburia <antonymburia10@gmail.com>
2022-03-08 04:24:01 +0300

    final commit

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SENDER_EMAIL = os.environ.get("MAIL_USERNAME")
+    
    
 
 
